0b5c0ead057bf3406da5d32efec6cec23abd1b58
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2020-02-21 03:49:41 -0500

    grid is working

--- pages/minsky.tsx
+++ pages/minsky.tsx
@@ -1,13 +1,24 @@
-import React, { FunctionComponent } from "react";
+import React, { FC } from "react";
 import Head from "next/head";
+import { styled } from "linaria/react";
+import Grid from "../components/atoms/Grid";
 
-const IndexPage: FunctionComponent = () => {
+const IndexPage: FC = () => {
     return (
         <div>
             <Head>
                 <title>Minsky | Lab</title>
             </Head>
-            <div>Hello</div>
+
+            <Grid container gap={"1em"}>
+                <Grid>A</Grid>
+                <Grid>B</Grid>
+                <Grid container columnsTemplate={"repeat(3, auto)"} rowsTemplate={"repeat(3, auto)"}>
+                    {[1, 2, 3, 4, 5, 6, 7, 8, 9].map(i => (
+                        <Grid>{`${i}`}</Grid>
+                    ))}
+                </Grid>
+            </Grid>
         </div>
     );
 };
