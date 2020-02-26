1879aafa02d4c70b71a82024f881d20c9ca87126
mariagnoriegag <maria.noriega@utec.edu.pe>
2020-02-26 03:05:08 -0500

    images are added

--- components/organisms/MinskyGive/giveYour.tsx
+++ components/organisms/MinskyGive/giveYour.tsx
@@ -2,54 +2,57 @@ import React from "react";
 import Grid from "../../atoms/Grid/v2";
 import { Text } from "../../atoms/Text";
 import { TextTypes } from "../../atoms/Text/constants";
-import {GiveYouCards,GiveUProps} from "../../molecules/Cards/giveYouCard";
+import { GiveYouCards, GiveUProps } from "../../molecules/Cards/giveYouCard";
 
 const DEBUG = false;
 
 // title: string,
 // icon: any,
 // body:string,
-const GiveYou = () => {
-    const items: GiveUProps[] = [
-        {
-            title: "High level of service",
-            body: "We respond to the needs and desires of customers, with the lowest possible cost and the highest level of quality and details.",
-            icon: "as",
-        },
-        {
-            title: "Constant maintenance",
-            body: "We keep abreast of the latest technologies, test them and verify their effectiveness in the development of projects.",
-            icon: "as",
-        },
-        {
-            title: "Open development for the community",
-            body: "Every time we take projects, micro technology and engines occur to us, which we make available to the community in order to improve the development of technology in the country.",
-            icon: "as",
-        },
-    ]
+const GiveYou = () => {
+	const items: GiveUProps[] = [
+		{
+			title: "High level of service",
+			body:
+				"We respond to the needs and desires of customers, with the lowest possible cost and the highest level of quality and details.",
+			icon: "as"
+		},
+		{
+			title: "Constant maintenance",
+			body:
+				"We keep abreast of the latest technologies, test them and verify their effectiveness in the development of projects.",
+			icon: "as"
+		},
+		{
+			title: "Open development for the community",
+			body:
+				"Every time we take projects, micro technology and engines occur to us, which we make available to the community in order to improve the development of technology in the country.",
+			icon: "as"
+		}
+	];
 
-    return (
-        <Grid debug={DEBUG} rowsTemplate={{ parts: 2, size:"auto" }}  colsTemplate={[{parts:1, size:"auto"},{parts:2, size:"1fr"},{parts:2, size:"1fr"}]} type={"grid"}>
-                <Grid rows={{from:1,how:1}} cols={{from:1,how:1}} type={"block"}>
-                    <Text alignText="start" type={TextTypes.title}>
-                        We give you
-                    </Text>
-                </Grid>
-                <Grid rows={{from:2,how:1}} cols={{from:1,how:1}} type={"block"}>
-                    {
-                        items.map((value)=>{
-                            return <GiveYouCards  body={value.body} icon={value.icon} title={value.title}></GiveYouCards>
-                        })
-                    }
+	return (
+		<Grid
+			debug={DEBUG}
+			rowsTemplate={{ parts: 2, size: "auto" }}
+			colsTemplate={[ { parts: 1, size: "auto" }, { parts: 2, size: "1fr" }, { parts: 2, size: "1fr" } ]}
+			type={"grid"}
+		>
+			<Grid rows={{ from: 1, how: 1 }} cols={{ from: 1, how: 1 }} type={"block"}>
+				<Text alignText="start" type={TextTypes.title}>
+					We give you
+				</Text>
+			</Grid>
+			<Grid rows={{ from: 2, how: 1 }} cols={{ from: 1, how: 1 }} type={"block"}>
+				{items.map(value => {
+					return <GiveYouCards body={value.body} icon={value.icon} title={value.title} />;
+				})}
+			</Grid>
+			<Grid rows={{ from: 1, how: 2 }} cols={{ from: 2, how: 1 }} type="flex" justifyContent="center">
+				<img src="/images/wegiveyou.png" alt={"We give you"} style={{ height: "100vh", width: "auto" }} />
+			</Grid>
+		</Grid>
+	);
+};
 
-                </Grid>
-                <Grid rows={{from:1,how:2}} cols={{from:2,how:1}} type="flex" justifyContent="center"> 
-                    <img src="/images/uplightbulb.png" alt="We give you" width="512px"  height="512px" ></img>
-                </Grid>
-        </Grid>
-    )
-
-}
-
-
-export {GiveYou};
+export { GiveYou };
