adc5736379091e62b5920e027d1fd925f76d4608
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2020-03-01 03:11:40 -0500

    with favicons and system configured

--- components/templates/BasicTemplate.tsx
+++ components/templates/BasicTemplate.tsx
@@ -33,7 +33,11 @@ const BasicTemplate = (props: any) => {
             `}</style>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+                <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
                 <meta
                     name="description"
                     content="Minsky is a Peruvian located company with the mision to change the technology reality of Peru"
