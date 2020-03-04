526f6b6aa662aab86889bc86d9d4e688b5f7564d
antoniott15 <antonio.toche@utec.edu.pe>
2020-03-04 12:10:49 -0500

    developers translate

--- pages/developers.tsx
+++ pages/developers.tsx
@@ -6,8 +6,10 @@ import Grid from "../components/atoms/Grid/v2";
 import { Body, Title, Simple } from "../components/atoms/Text/v2";
 import DevRegisterForm from "../components/organisms/DevsRegisterForm";
 import { Footer } from "../components/organisms/MinskyFooter";
+import NextI18NextInstance from "../general/i18n";
 
 const DevelopersPage: NextPage = () => {
+    const [t, i18n] = NextI18NextInstance.useTranslation("developer");
     return (
         <>
             <Head>
@@ -18,22 +20,17 @@ const DevelopersPage: NextPage = () => {
             <Grid p={[{ x: "2rem" }, { x: "15vw" }]} m={{ top: "6rem" }}>
                 <div>
                     <Title size={"3rem"} bold>
-                        Hi Developers
+                        {t("greetings")}
                     </Title>
                 </div>
                 <div style={{ marginTop: "1.5rem" }}>
                     <Body size={"1.3rem"}>
-                        At Minsky, we heavily think that we need to work with the community and promote open knowledge
-                        for its. We think that way have a powerful tool to change and route our nation to a
-                        technology-based future. We want to know more developers (and not) interested in use and enhance
-                        this model {"-"}based on open source and community driven{"-"} following the only goal to change
-                        the technology reality of Peru.
+                       {t("body")}
                     </Body>
                 </div>
                 <div style={{ marginTop: "1.5rem" }}>
                     <Simple size={"1rem"}>
-                        If you’re interested in work side by side with us, please fill the following form, we are very
-                        excited to meet you.
+                        {t("interested")}
                     </Simple>
                 </div>
                 <div style={{ marginTop: "1.5rem" }}>
