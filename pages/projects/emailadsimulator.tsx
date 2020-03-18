b459d02cc1db30359a90b4e0116b34308922d9a8
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2020-03-18 10:20:55 -0500

    new email simulator page was created

---
+++ pages/projects/emailadsimulator.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from "react";
+import { NextPage } from "next";
+
+const SimulatorPage: FC<NextPage> = (props: NextPage) => {
+    return <div>Simulator Page</div>;
+};
+
+export default SimulatorPage;
